8ef65697cbe13256182a872e2574eb6e845355ba
BlackJack7878 <oleg55862@gmail.com>
2017-03-15 21:48:43 +0200

    profile/home pages update
    - pagination for home page
    - scroll btn to goods for home page
    - profile/archive header changes

--- src/js/partials/home.js
+++ src/js/partials/home.js
@@ -11,4 +11,9 @@ $(document).ready(function() {
 		}
 	});
 
+	$('.arrow').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({scrollTop:$(target).position().top - 100}, 2000);
+	});
+
 });
